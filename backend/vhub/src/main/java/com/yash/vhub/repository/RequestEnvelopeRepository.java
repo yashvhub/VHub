fc1c462773498ad01f1bd7b0c603fdaf844d0528
Caleb Reiter <caleb@reiters.org>
2019-08-02 13:43:54 -0500

    added searching by requester name on requestenvelope

--- backend/vhub/src/main/java/com/yash/vhub/repository/RequestEnvelopeRepository.java
+++ backend/vhub/src/main/java/com/yash/vhub/repository/RequestEnvelopeRepository.java
@@ -1,11 +1,17 @@
 package com.yash.vhub.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.yash.vhub.domain.RequestEnvelope;
 
 @RepositoryRestResource(path="request-envelopes")
 public interface RequestEnvelopeRepository extends JpaRepository<RequestEnvelope, Long> {
-
+	
+	@Query("SELECT e FROM RequestEnvelope e INNER JOIN e.requester r WHERE CONCAT(r.firstName, ' ', r.lastName) LIKE %:name%")
+	Set<RequestEnvelope> findByRequesterName(@Param("name") String name);
 }
