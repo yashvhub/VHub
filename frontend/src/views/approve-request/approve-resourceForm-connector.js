7e9aeb6d04faed65ca4fb194bd1e62c71020d54b
Caleb Reiter <caleb@reiters.org>
2019-08-02 13:10:47 -0500

    fix Approve Request Resource Forms

---
+++ frontend/src/views/approve-request/approve-resourceForm-connector.js
@@ -0,0 +1,16 @@
+import {connect} from 'react-redux';
+import ApproveResourceForm from './approve-resourceForm';
+import {addNewSkill} from '../../action-creators/request';
+
+function mapStateToProps(state, ownProps){
+    let {hourlyRate, yearsOfExperience, count, skills} = state.requestEnvelope.item.resourceRequests.find(({id}) => id === ownProps.id)
+    return{
+        hourlyRate,
+        yearsOfExperience,
+        count,
+        skills: skills.map(({skill}) => skill)
+    }
+    
+}
+
+export default connect(mapStateToProps)(ApproveResourceForm);
