f1987e9f1327845eebc75d99b7d21c7996be1ceb
Lhito <crizaxo@gmail.com>
2019-08-26 14:28:56 -0700

    fixed a merge error

--- frontend/src/API/request-envelopes.js
+++ frontend/src/API/request-envelopes.js
@@ -1,4 +1,4 @@
-import API, {Repository} from '.';
+import API, { Repository } from '.';
 import ResourceRequests from './resource-requests';
 import Skills from './skills';
 import Locations from './locations';
@@ -6,9 +6,8 @@ import Users from './users';
 import Comments from './request-comments'
 
 class RequestEnvelopeRepository extends Repository {
-    async post(data, config={headers:{'Content-Type': 'application/json'}}){
-        try{
-            console.log("here's the data you fuck", data)
+    async post(data, config = { headers: { 'Content-Type': 'application/json' } }) {
+        try {
             const response = await API.post(this.url, data.baseRequest, config);
 
             const locationPref = {
@@ -17,14 +16,14 @@ class RequestEnvelopeRepository extends Repository {
                 stateOrProvince: data.locationStatePref,
             }
 
-            const locationResponse = await API.post(`${Locations.getPath()}/`, locationPref, {headers:{'Content-Type': 'application/json'}})
+            const locationResponse = await API.post(`${Locations.getPath()}/`, locationPref, { headers: { 'Content-Type': 'application/json' } })
 
-                const requestPath = `${this.getPath()}/${response.data.id}/`
+            const requestPath = `${this.getPath()}/${response.data.id}/`
 
-                const approversUriList = data.approver.map(approve =>
-                    `${Users.getPath()}/${approve}`)
+            const approversUriList = data.approver.map(approve =>
+                `${Users.getPath()}/${approve}`)
 
-                const approverResponse = API.put(`${requestPath}/approvers`, 
+            const approverResponse = API.put(`${requestPath}/approvers`,
                 approversUriList.join('\n'),
                 {
                     headers: {
@@ -32,10 +31,10 @@ class RequestEnvelopeRepository extends Repository {
                     }
                 });
 
-                const interviewersUriList =  data.interviewers.map(interviewer =>
-                    `${Users.getPath()}/${interviewer}`)
+            const interviewersUriList = data.interviewers.map(interviewer =>
+                `${Users.getPath()}/${interviewer}`)
 
-                const interviewersResponse = API.put(`${requestPath}/interviewers`, 
+            const interviewersResponse = API.put(`${requestPath}/interviewers`,
                 interviewersUriList.join('\n'),
                 {
                     headers: {
@@ -43,75 +42,62 @@ class RequestEnvelopeRepository extends Repository {
                     }
                 });
 
-                const requestEnvelope ={
-                    requestStatus: `${requestPath}requestStatuses/1`, //neeed to be defaulted upon creation
-                    requester: `${requestPath}requester/${data.requester}`,
-                    proposalType: `${requestPath}proposalType/1`, //hard coded until internal or external is implemented
-                    locationPreference: `${requestPath}locationPreference/${locationResponse.data.id}`
+            const requestEnvelope = {
+                requestStatus: `${requestPath}requestStatuses/1`, //neeed to be defaulted upon creation
+                requester: `${requestPath}requester/${data.requester}`,
+                proposalType: `${requestPath}proposalType/1`, //hard coded until internal or external is implemented
+                locationPreference: `${requestPath}locationPreference/${locationResponse.data.id}`
+            }
+            const response2 = await API.patch(`${requestPath}`, requestEnvelope, { ...config, headers: { 'Content-Type': 'application/json' } })
+            data.resources.map(async (resource) => {
+                let resourceRequest = {
+                    count: resource.number,
+                    hourlyRate: resource.compensation,
+                    yearsOfExperience: resource.experience,
+                    requestEnvelopeId: response.data.id,
                 }
-                const response2 = await API.patch(`${requestPath}`, requestEnvelope, {...config, headers:{'Content-Type': 'application/json'}})
-                data.resources.map(async (resource)=>{
-                    let resourceRequest = {
-                        count: resource.number,
-                        hourlyRate: resource.compensation,
-                        yearsOfExperience: resource.experience,
-                        requestEnvelopeId: response.data.id,
-                    }
-                    const resourceRequestResponse = await API.post(`${ResourceRequests.getPath()}/`, resourceRequest, {headers:{'Content-Type': 'application/json'}})
-                    
-                    const skillUriList = resource.skills.map(skill =>
-                        `${Skills.getPath()}/${skill}`)
-    
-                    const skillsResponse = await API.put(`${ResourceRequests.getPath()}/${resourceRequestResponse.data.id}/skills`, 
-                    skillUriList.join('\n'),
-                    {
-                        headers: {
-                            'Content-Type': 'text/uri-list'
-                        }
-                    });
-                })
-                
-                
+                const resourceRequestResponse = await API.post(`${ResourceRequests.getPath()}/`, resourceRequest, { headers: { 'Content-Type': 'application/json' } })
+
                 const skillUriList = data.resources[0].skills.map(skill =>
                     `${Skills.getPath()}/${skill}`)
-                    
-                    const skillsResponse = await API.put(`${ResourceRequests.getPath()}/${resourceRequestResponse.data.id}/skills`, 
+
+                const skillsResponse = await API.put(`${ResourceRequests.getPath()}/${resourceRequestResponse.data.id}/skills`,
                     skillUriList.join('\n'),
                     {
                         headers: {
                             'Content-Type': 'text/uri-list'
                         }
                     });
-                    console.log(skillsResponse)
-                    
-                    
-                    let today = new Date()
-                    const dd = String(today.getDate()).padStart(2, '0');
-                    const mm = String(today.getMonth() + 1).padStart(2, '0');
-                    const yyyy = today.getFullYear();
-                    
-                    today = yyyy + '-' + mm + '-' + dd;
-                    
-                    const comment = {
-                        requestId: response.data.id,
-                        author: `${requestPath}requester/${data.requester}`,
-                        comment: data.comments,
-                        createdAt: today,
-                    }
-                    
-                    const commentResponse = await API.post(`${Comments.getPath()}/`, comment, {headers:{'Content-Type': 'application/json'}})
-                    console.log("comment response",commentResponse)
-                    
-                    if(response, response2, interviewersResponse, approverResponse, locationResponse){
-                        return{success:true}
-                    }
-                    
-                }catch(e){
+                console.log(skillsResponse)
+            })
+
+            let today = new Date()
+            const dd = String(today.getDate()).padStart(2, '0');
+            const mm = String(today.getMonth() + 1).padStart(2, '0');
+            const yyyy = today.getFullYear();
+
+            today = yyyy + '-' + mm + '-' + dd;
+
+            const comment = {
+                requestId: response.data.id,
+                author: `${requestPath}requester/${data.requester}`,
+                comment: data.comments,
+                createdAt: today,
+            }
+
+            const commentResponse = await API.post(`${Comments.getPath()}/`, comment, { headers: { 'Content-Type': 'application/json' } })
+            console.log("comment response", commentResponse)
+
+            if (response, response2, interviewersResponse, approverResponse, locationResponse) {
+                return { success: true }
+            }
+
+        } catch (e) {
             console.error(e)
         }
     }
-    
-    async getByName(name, config={}) {
+
+    async getByName(name, config = {}) {
         try {
             const response = await API.get(`${this.url}/search/findByRequesterNameByRequestDateDesc`, {
                 ...config,
@@ -126,20 +112,20 @@ class RequestEnvelopeRepository extends Repository {
         }
     }
 
-    async approvePatch(data, id, config={}) {
+    async approvePatch(data, id, config = {}) {
         try {
-            if(data.id){
+            if (data.id) {
                 await API.patch(`${this.url}/${id}`, {
                     requestEnvelopeId: data.id
                 })
             }
-            if(data){
+            if (data) {
                 await API.put(`${this.getPath()}/${id}/requestStatus`, `${this.getPath()}/${id}/requestStatuses/${2}`,
-                {headers: {'Content-Type': 'text/uri-list'}});
+                    { headers: { 'Content-Type': 'text/uri-list' } });
                 return [true, null];
             }
         }
-        catch(e) {
+        catch (e) {
             console.error(e);
             return [null, 'Update Failed.']
         }
